145807335b5f5d2c1c879b6f0559bd28f425584e
sreenath <sreenathmv2002@gmail.com>
2023-10-02 23:44:34 +0530

    crud app

---
+++ src/main/java/com/example/Springcrudappproject/SpringcrudappprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springcrudappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrudappprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcrudappprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springcrudappproject/SpringcrudappprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springcrudappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcrudappprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
